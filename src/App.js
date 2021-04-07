2f1242d1c5712565616c55358c766e1e7a43d489
”sovsianykov” <swebproger@gmail.com>
2021-04-07 17:16:53 +0300

    well done

--- src/App.js
+++ src/App.js
@@ -1,72 +1,97 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
-import {Button} from "@material-ui/core";
+import { Button } from "@material-ui/core";
 import PlayingModal from "./components/PlayingModal/PlaingModal";
 
 function App() {
-    let today = new Date();
-    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    const [balance, setBalance ] = useState(0);
-    const [spinRow , setSpinRow] = useState([{  slot1: 0, slot2: 0, slot3: 0, time : time }]);
-    const [currentSpin, setCurrentSpin ] = useState({  slot1: 0, slot2: 0, slot3: 0, time : time })
-    const [open, setOpen] = React.useState(false);
+  let today = new Date();
+  let time =
+    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  const [balance, setBalance] = useState(0);
+  const [spinRow, setSpinRow] = useState([
+    { slot1: 0, slot2: 0, slot3: 0, time: time },
+  ]);
+  const [currentSpin, setCurrentSpin] = useState({
+    slot1: 0,
+    slot2: 0,
+    slot3: 0,
+    time: time,
+  });
+  const [open, setOpen] = React.useState(false);
 
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
-      const handleOpen = () => {
-        setOpen(true);
-    };
+  const getSpin = () => {
+    let spin1 = Math.floor(Math.random() * 9) + 1;
+    let spin2 = Math.floor(Math.random() * 9) + 1;
+    let spin3 = Math.floor(Math.random() * 9) + 1;
 
-    const getSpin = () =>{
-        let spin1 = Math.floor(Math.random() * 9) +1;
-        let spin2 = Math.floor(Math.random() * 9) +1;
-        let spin3 = Math.floor(Math.random() * 9) +1;
-
-        let spinArr = [{slot1: spin1, slot2: spin2, slot3: spin3, time : time  }]
-        setCurrentSpin(spinArr[0])
-        setBalance(balance -1)
-        setSpinRow(spinRow.concat(spinArr))
-        if (spin1 === spin2 && spin2 === spin3 ) { setBalance(balance + 5)  }
-        else if ( spin1 === spin2 || spin2 === spin3 || spin1 === spin3 ) {setBalance(balance + 0.5)}
+    let spinArr = [{ slot1: spin1, slot2: spin2, slot3: spin3, time: time }];
+    setCurrentSpin(spinArr[0]);
+    setBalance(balance - 1);
+    setSpinRow(spinRow.concat(spinArr));
+    if (spin1 === spin2 && spin2 === spin3) {
+      setBalance(balance + 5);
+    } else if (spin1 === spin2 || spin2 === spin3 || spin1 === spin3) {
+      setBalance(balance + 0.5);
     }
-    const test777 =() =>{
-        let spin1 =  7;
-        let spin2 = 7;
-        let spin3 = 7;
+  };
+  const test777 = () => {
+    let spin1 = 7;
+    let spin2 = 7;
+    let spin3 = 7;
 
-        let spinArr = [{slot1: spin1, slot2: spin2, slot3: spin3, time : time  }]
-        setCurrentSpin(spinArr[0])
-        setBalance(balance -1)
-        setSpinRow(spinRow.concat(spinArr))
-        if (spin1 === spin2 && spin2 === spin3 ) { setBalance(balance + 5)  }
-        else if ( spin1 === spin2 || spin2 === spin3 || spin1 === spin3 ) {setBalance(balance + 0.5)}
-
+    let spinArr = [{ slot1: spin1, slot2: spin2, slot3: spin3, time: time }];
+    setCurrentSpin(spinArr[0]);
+    setBalance(balance - 1);
+    setSpinRow(spinRow.concat(spinArr));
+    if (spin1 === spin2 && spin2 === spin3) {
+      setBalance(balance + 5);
+    } else if (spin1 === spin2 || spin2 === spin3 || spin1 === spin3) {
+      setBalance(balance + 0.5);
     }
-    useEffect(() =>{
-        const data = localStorage.getItem('my-store')
-        if (data) {  setSpinRow(JSON.parse(data))}
-        const data2 = localStorage.getItem('my-store2')
-        if (data2) { setBalance(JSON.parse(data2))}
-    },[])
-    useEffect(() =>{
-        localStorage.setItem('my-store',JSON.stringify(spinRow))
-        localStorage.setItem('my-store2',JSON.stringify(balance))
-    })
+  };
+  useEffect(() => {
+    const data = localStorage.getItem("my-store");
+    if (data) {
+      setSpinRow(JSON.parse(data));
+    }
+    const data2 = localStorage.getItem("my-store2");
+    if (data2) {
+      setBalance(JSON.parse(data2));
+    }
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("my-store", JSON.stringify(spinRow));
+    localStorage.setItem("my-store2", JSON.stringify(balance));
+  });
 
-
-    return (
+  return (
     <div className="App">
-      <Header balance = {balance} setBalance={setBalance}  />
-        <Button variant='contained' className='btn-start' color='secondary' onClick={handleOpen} >
-            START TO PLAY
-        </Button>
-        <PlayingModal open = {open} setOpen = {setOpen} currentSpin = {currentSpin} getSpin = {getSpin} test777={test777} />
-      <Content spinRow = { spinRow }/>
-      <Footer/>
+      <Header balance={balance} setBalance={setBalance} />
+      <Button
+        variant="contained"
+        className="btn-start"
+        color="secondary"
+        onClick={handleOpen}
+      >
+        START TO PLAY
+      </Button>
+      <PlayingModal
+        open={open}
+        setOpen={setOpen}
+        currentSpin={currentSpin}
+        getSpin={getSpin}
+        test777={test777}
+      />
+      <Content spinRow={spinRow} />
+      <Footer />
     </div>
   );
 }
-
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,100 +1,103 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import { useStyles } from "./styles";
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import {FormControl, FormGroup, FormLabel} from "@material-ui/core";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import { FormControl, FormGroup, FormLabel } from "@material-ui/core";
 import Modal from "@material-ui/core/Modal";
 
 function rand() {
-    return Math.round(Math.random() * 20) - 10;
+  return Math.round(Math.random() * 20) - 10;
 }
 
 function getModalStyle() {
-    const top = 50 + rand();
-    const left = 50 + rand();
+  const top = 50 + rand();
+  const left = 50 + rand();
 
-    return {
-        top: `${top}%`,
-        left: `${left}%`,
-        transform: `translate(-${top}%, -${left}%)`,
-    };
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
 }
 
+export default function Header({ balance, setBalance }) {
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const [nickname, setNickname] = useState("guest");
 
-
-
-export default function Header({balance,setBalance}) {
-    const classes = useStyles();
-    const [modalStyle] = React.useState(getModalStyle);
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => {
-        setOpen(true);
-    };
-    const [ nickname , setNickname ] = useState('guest');
-
-
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-   const handleLogin =(e)=>{
-       setNickname(e.target.value)
-       setBalance(99)
-   }
-    const handleLogout =()=>{
-        setNickname('guest')
-        setBalance('')
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleLogin = (e) => {
+    setNickname(e.target.value);
+    setBalance(99);
+  };
+  const handleLogout = () => {
+    setNickname("guest");
+    setBalance("");
+  };
+  useEffect(() => {
+    const data = localStorage.getItem("my-store3");
+    if (data) {
+      setNickname(JSON.parse(data));
     }
-    useEffect(() =>{
-        const data = localStorage.getItem('my-store3')
-        if (data) {  setNickname(JSON.parse(data))}
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("my-store3", JSON.stringify(nickname));
+  });
 
-    },[])
-    useEffect(() =>{
-        localStorage.setItem('my-store3',JSON.stringify(nickname))
-    })
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <FormGroup>
+        <FormControl>
+          <FormLabel name="login">login</FormLabel>
+          <input
+            type="text"
+            name="login"
+            onChange={(e) => {
+              handleLogin(e);
+            }}
+          />
+        </FormControl>
+      </FormGroup>
+    </div>
+  );
 
-
-    const body = (
-        <div style={modalStyle} className={classes.paper}>
-            <FormGroup>
-                <FormControl>
-                    <FormLabel name='login'>login</FormLabel>
-                    <input type="text" name='login' onChange={ e=>{handleLogin(e)}} />
-                </FormControl>
-            </FormGroup>
-        </div>
-    );
-
-
-    return (
-        <div className={classes.root}>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        PLAY THE GAME
-                    </Typography>
-                    <Modal
-                        open={open}
-                        onClose={handleClose}
-                    >
-                        {body}
-
-                    </Modal>
-                    {nickname}  ${balance}
-                       <Button color="inherit" onClick={handleOpen} >Login</Button>
-                       <Button color="inherit" onClick={handleLogout} >Logout</Button>
-
-
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            PLAY THE GAME
+          </Typography>
+          <Modal open={open} onClose={handleClose}>
+            {body}
+          </Modal>
+          {nickname} ${balance}
+          <Button color="inherit" onClick={handleOpen}>
+            Login
+          </Button>
+          <Button color="inherit" onClick={handleLogout}>
+            Logout
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
